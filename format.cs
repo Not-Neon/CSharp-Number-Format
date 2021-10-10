3f8b703cc1d605733ed41ab87a7765b4aa628ff1
Neon <82705218+NotNeonDEV@users.noreply.github.com>
2021-10-10 09:03:06 +0530

    Add files via upload

---
+++ format.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace Num_Format_CSharp
+{
+    class Program
+    {
+        static void Format()
+        {
+            Console.Write("Enter a number for formatting --> ");
+            string input = Console.ReadLine();
+            double num;
+            Double.TryParse(input, out num);
+            string place = "";
+            
+            if (num <= 999)
+            {
+                place = "";
+            }
+            else if (999 <= num && num < 99999)
+            {
+                double x = num / 10;
+                num = x / 100;
+                place = "Thousand";
+            }
+            else if (999999 <= num && num < 9999999999)
+            {
+                double x = num / 10000;
+                num = x / 100;
+                place = "Million";
+            }
+            else if (9999999999 <= num && num < 999999999999)
+            {
+                double x = num / 10000000;
+                num = x / 100;
+                place = "Billion";
+            }
+
+            Console.WriteLine($"\n{num} {place}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "C# Number Format Console";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Start Program. [Y]es or [N]o?");
+            string ask = Console.ReadLine();
+            
+            if (ask == "Y")
+            {
+                Format();
+            }
+            else
+            {
+                Console.WriteLine("Exiting Program...");
+            }
+        }
+    }
+}
